ae31ead8f8285fdd30b88e84f35c7ad26bd5c402
tulika5 <58421263+tulika5@users.noreply.github.com>
2020-09-20 16:11:51 +0530

    Create HelloWorld.scala

---
+++ HelloWorld.scala
@@ -0,0 +1,8 @@
+/* object Main inherits the main method of App trait.
+app is a helper code
+a trait is like interface of java, but it can have partial implementation also
+*/
+
+object Main extends App {
+	print("Hello Scala")
+}
